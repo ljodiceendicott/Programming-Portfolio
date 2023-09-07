cb1151d6d8ba0782f599f9824fc5cfa0b95ce800
ljodice <>
2023-09-07 19:41:52 -0400

    edited links to be added as the children

--- src/pages/components/ItemCard.tsx
+++ src/pages/components/ItemCard.tsx
@@ -28,18 +28,7 @@ const ItemCard: React.FC<ItemCardProps> = ({
       <h1 className="text-2xl font-bold text-black">{title}</h1>
       <h4 className="font-semibold text-slate-800">{company}</h4>
       <div className="flex-wrap text-black">{description}</div>
-      {/* <div>
-        <h4 className="font-bold text-green-700">Overall Project Takeaway</h4>
-        {postiveTakaway}
-      </div>
-      <div>
-        <h4 className="text-amber-700">What I Learned From Project</h4>
-        {learningops}
-      </div> */}
-      <div className="text-right">{children}</div>
-      <div className="text-right text-black">
-        {/* <Link href={repoLink}>Link to Repository</Link> */}
-      </div>
+      <div className="flex-row text-right">{children}</div>
     </div>
   );
 };
