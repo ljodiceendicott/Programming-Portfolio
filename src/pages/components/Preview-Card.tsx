dbaf2c37acae468830188a3fe21783dab851d9ec
ljodice <>
2023-10-05 23:21:20 -0400

    made previewcard; fixed display on homepage

---
+++ src/pages/components/Preview-Card.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+
+interface ItemCardProps {
+  className?: string;
+  children?: React.ReactNode;
+  title: string;
+  company?: string;
+  description: string;
+  repoLink: string;
+  liveLink?: string;
+  postiveTakaway?: string;
+  learningops?: string;
+  pageLink: string;
+  technologies: Array<string>;
+}
+
+const ItemCard: React.FC<ItemCardProps> = ({
+  className,
+  children,
+  title,
+  company,
+  description,
+  postiveTakaway,
+  learningops,
+  technologies,
+  repoLink,
+  pageLink,
+}) => {
+  return (
+    <>
+      <div className="preview-card">
+        <h1 className="text-2xl font-bold text-black">{title}</h1>
+        <div id="techcontainer" className="flex justify-center space-x-3">
+          {technologies.map((tech, idx) => (
+            <div key={idx} id={tech + idx}>
+              {tech}
+            </div>
+          ))}
+        </div>
+        <h4 className="font-semibold text-slate-800">{company}</h4>
+        <div className="flex-wrap text-black">{description}</div>
+        <div className="flex-row text-right">{children}</div>
+        <div id="links">
+          <Link href={repoLink}>Link to Github Repo</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+export default ItemCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Header from "~/pages/components/Header";
+import PreviewCard from "./components/Preview-Card";
 
 import resume from "~/pages/resume.json";
 import { api } from "~/utils/api";
-import ItemCard from "./components/ItemCard";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -18,16 +18,24 @@ export default function Home() {
           find opportinities in the field of Software Engineering.
           <br />
           This is my portfolio, which contains my previous projects,
-          experiences, as well as other things related to my development of
-          software engineering skills.
+          experiences, as well as other things related to the development of my
+          skills as a Software Engineer.
+          <br />I have a strong interest in working anywhere along the
+          techstack. <br />I am also a person who makes connections with my
+          coworkers making them laugh while also being professional and reliable
         </h4>
         <h2 className="py-10 pl-4 text-left text-5xl font-bold">Projects</h2>
-        <div id="projects" className="columns-2 space-y-12">
+        <div
+          id="projects"
+          className="columns-2 space-y-12 max-md:w-screen max-md:columns-1"
+        >
           {cv.projects.map((project, index) => (
-            <ItemCard
+            <PreviewCard
               key={index}
               title={project.name}
               description={project.description}
+              repoLink={project.githublink}
+              technologies={project.technologies}
               pageLink="/"
             />
           ))}
