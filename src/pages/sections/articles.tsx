f33a5079146a660c7348f286b1a0b64206880de1
ljodice <>
2024-10-23 11:03:03 -0400

    rm array from outer json of articles, added code to read the articles from this json file

--- src/pages/sections/articles.tsx
+++ src/pages/sections/articles.tsx
@@ -1,11 +1,11 @@
-import Article from "~/pages/components/article";
 import Header from "~/pages/components/Header";
 
 import articlelist from "../combined_article.json";
+import Article from "../components/article";
 
 export default function articles() {
 
-const articles = articlelist.toString();
+const articlelistjson = articlelist.data.viewer.latestPostsConnection.postPreviews;
 
 const unixtext = 1709699255403;
 let time = new Date(unixtext);
@@ -14,14 +14,23 @@ let time = new Date(unixtext);
     <div>
       <Header />
       <div id="content" className="mt-12 w-1/2 space-y-20 text-left">
-        {/* New component for articles, May require refactoring of item card */}
-        <Article 
+        {/* This will need to use map as there will be multiple articles that this is pulling from*/}
+        {/* <Article 
         article_url=""
         title="Test Article"
         subtitle="this is a test"
         datePub= {time.toLocaleDateString()}
-              />
-      
+              /> */}
+        {articlelistjson.map((art, index) => (
+          // Need to retrieve Post so it will be art.post
+          <Article
+            key = {index}
+            article_url= {art.post.mediumUrl}
+            title={art.post.title}
+            subtitle={art.post.previewContent.subtitle}
+            datePub={new Date(art.post.firstPublishedAt).toLocaleDateString()}
+          />
+        ))}
       </div>
     </div>
   );
